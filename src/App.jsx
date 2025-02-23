7b9d03540069bb77549dfefdc1698afbdbfb5acf
Sultana Yeasmin <EO005092@bifpcl.net>
2025-02-23 20:27:20 +0600

    updated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+              
+      <h1 className="text-green-600 text-center text-3xl my-[10%] text-bold">Assignment-12</h1>
+     
+    </>
+  )
+}
+
+export default App
