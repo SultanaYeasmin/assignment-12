f35e8761ab1f1791ae08c77763d79dd6ca0eee5b
BIFPCL_PC <sy060911@bifpcl.com>
2025-07-20 12:05:27 +0600

    updated

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,26 +1,28 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import { useEffect, useState } from 'react';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import useAuth from '../../../hooks/useAuth';
 
 
 
-const CheckoutForm = ({price}) => {
+const CheckoutForm = ({ price }) => {
     const [error, setError] = useState('')
     const [clientSecret, setClientSecret] = useState('')
+    const [transactionId, setTransactionId] = useState('');
     const stripe = useStripe();
     const elements = useElements();
     const axiosSecure = useAxiosSecure();
-   
+    const { user } = useAuth();
     useEffect(() => {
         if (price) {
             axiosSecure.post('/create-payment-intent', { price: price })
                 .then(res => {
-                    console.log("client secret: ",res.data.clientSecret)
+                    console.log("client secret: ", res.data.clientSecret)
                     setClientSecret(res.data.clientSecret)
                 })
 
         }
-    }, [price])
+    }, [price, axiosSecure])
 
     const handleSubmit = async (event) => {
         // Block native form submission.
@@ -54,11 +56,61 @@ const CheckoutForm = ({price}) => {
             console.log('[PaymentMethod]', paymentMethod);
             setError('')
         }
-
-
         
-    };
+        //confirm card-payment
+        const { paymentIntent, 
+            error: cardConfirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+               payment_method: {
+                    card: card,
+                    billing_details: {
+                        email: user?.email || 'anonymous',
+                        name: user?.displayName || 'anonymous',
+                    },
+                },
+            },
+        );
+        if (cardConfirmError) {
+            console.log('confirm error!', cardConfirmError)
+        } else {
+            console.log('payment intent:', paymentIntent)
+        }
 
+  
+    if (paymentIntent?.status === 'succeeded') {
+        console.log('transaction id', paymentIntent.id);
+        setTransactionId(paymentIntent.id);
+
+
+        //now save the payment in the database
+
+        // const payment = {
+        //     email: user?.email,
+        //     price: totalPrice,
+        //     transactionId: paymentIntent.id,
+        //     date: new Date(),
+
+        //     status: 'pending',
+
+        // }
+
+        // const res = await axiosSecure.post('/payments', payment)
+
+        // console.log('payment saved to db!', res.data);
+        // refetch()
+        // if (res.data?.paymentResult?.insertedId) {
+        //     Swal.fire({
+        //         position: "top-end",
+        //         icon: "success",
+        //         title: `thank you for payment!`,
+        //         showConfirmButton: false,
+        //         timer: 1500
+        //     });
+        //     navigate('/dashboard/paymentHistory')
+        // }
+    }  
+};
 
     return (
         <form onSubmit={handleSubmit} className='p-2'>
@@ -84,6 +136,11 @@ const CheckoutForm = ({price}) => {
             <p className='text-red-400 font-bold '>
                 {error}
             </p>
+              {
+                transactionId && <p className='text-green-800'>
+                    Your transaction id is {transactionId}
+                </p>
+            }
         </form>
     );
 };
